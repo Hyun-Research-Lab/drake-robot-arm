2883437d3f16539218b0da40c95105a48afadc70
Darin Tsai <darint916@gmail.com>
2023-09-08 14:13:43 -0400

    launchable robot arm

---
+++ scripts/libs/meshcat_utils.py
@@ -0,0 +1,180 @@
+from IPython.display import display, HTML, Javascript
+from pydrake.geometry import Meshcat, Cylinder, Rgba, Sphere
+# from pydrake.perception import PointCloud, Fields, BaseField
+# from pydrake.solvers import BoundingBoxConstraint
+import time 
+import numpy as np
+
+# imports for the pose sliders
+from collections import namedtuple
+from pydrake.common.value import AbstractValue
+from pydrake.math import RollPitchYaw, RigidTransform, RotationMatrix
+from pydrake.systems.framework import LeafSystem, PublishEvent
+
+# imports for the joint sliders
+# from pydrake.multibody.tree import JointIndex
+# from pydrake.systems.framework import PublishEvent, VectorSystem
+
+# from manipulation import running_as_notebook
+
+def StartMeshcat(open_window=False):
+    """
+    A wrapper around the Meshcat constructor that supports Deepnote and Google
+    Colab via ngrok when necessary.
+    """
+    meshcat = Meshcat(8080)
+    web_url = meshcat.web_url()
+    display(
+        HTML('Meshcat is now available at '
+             f'<a href="{web_url}">{web_url}</a>'))
+    if open_window:
+        import webbrowser
+        webbrowser.open(web_url)
+        return meshcat
+
+
+class MeshcatPoseSliders(LeafSystem):
+    """
+    Provides a set of ipywidget sliders (to be used in a Jupyter notebook) with
+    one slider for each of roll, pitch, yaw, x, y, and z.  This can be used,
+    for instance, as an interface to teleoperate the end-effector of a robot.
+
+    .. pydrake_system::
+
+        name: PoseSliders
+        output_ports:
+        - pose
+    """
+    # TODO(russt): Use namedtuple defaults parameter once we are Python >= 3.7.
+    Visible = namedtuple("Visible", ("roll", "pitch", "yaw", "x", "y", "z"))
+    Visible.__new__.__defaults__ = (True, True, True, True, True, True)
+    MinRange = namedtuple("MinRange", ("roll", "pitch", "yaw", "x", "y", "z"))
+    MinRange.__new__.__defaults__ = (-np.pi, -np.pi, -np.pi, -1.0, -1.0, -1.0)
+    MaxRange = namedtuple("MaxRange", ("roll", "pitch", "yaw", "x", "y", "z"))
+    MaxRange.__new__.__defaults__ = (np.pi, np.pi, np.pi, 1.0, 1.0, 1.0)
+    Value = namedtuple("Value", ("roll", "pitch", "yaw", "x", "y", "z"))
+    Value.__new__.__defaults__ = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+
+    def __init__(self,
+                 meshcat,
+                 visible=Visible(),
+                 min_range=MinRange(),
+                 max_range=MaxRange(),
+                 value=Value()):
+        """
+        Args:
+            meshcat: A Meshcat instance.
+            visible: An object with boolean elements for 'roll', 'pitch',
+                     'yaw', 'x', 'y', 'z'; the intention is for this to be the
+                     PoseSliders.Visible() namedtuple.  Defaults to all true.
+            min_range, max_range, value: Objects with float values for 'roll',
+                      'pitch', 'yaw', 'x', 'y', 'z'; the intention is for the
+                      caller to use the PoseSliders.MinRange, MaxRange, and
+                      Value namedtuples.  See those tuples for default values.
+        """
+        LeafSystem.__init__(self)
+        port = self.DeclareAbstractOutputPort(
+            "pose", lambda: AbstractValue.Make(RigidTransform()),
+            self.DoCalcOutput)
+
+        # The widgets themselves have undeclared state.  For now, we accept it,
+        # and simply disable caching on the output port.
+        # TODO(russt): consider implementing the more elaborate methods seen
+        # in, e.g., LcmMessageSubscriber.
+        port.disable_caching_by_default()
+
+        self._meshcat = meshcat
+        self._visible = visible
+        self._value = list(value)
+
+        for i in range(6):
+            if visible[i]:
+                meshcat.AddSlider(min=min_range[i],
+                                  max=max_range[i],
+                                  value=value[i],
+                                  step=0.01,
+                                  name=value._fields[i])
+
+    def __del__(self):
+        for s in ['roll', 'pitch', 'yaw', 'x', 'y', 'z']:
+            if self._visible[s]:
+                self._meshcat.DeleteSlider(s)
+
+    def SetPose(self, pose):
+        """
+        Sets the current value of the sliders.
+
+        Args:
+            pose: Any viable argument for the RigidTransform
+                  constructor.
+        """
+        tf = RigidTransform(pose)
+        self.SetRpy(RollPitchYaw(tf.rotation()))
+        self.SetXyz(tf.translation())
+
+    def SetRpy(self, rpy):
+        """
+        Sets the current value of the sliders for roll, pitch, and yaw.
+
+        Args:
+            rpy: An instance of drake.math.RollPitchYaw
+        """
+        self._value[0] = rpy.roll_angle()
+        self._value[1] = rpy.pitch_angle()
+        self._value[2] = rpy.yaw_angle()
+        for i in range(3):
+            if self._visible[i]:
+                self._meshcat.SetSliderValue(self._visible._fields[i],
+                                             self._value[i])
+
+    def SetXyz(self, xyz):
+        """
+        Sets the current value of the sliders for x, y, and z.
+
+        Args:
+            xyz: A 3 element iterable object with x, y, z.
+        """
+        self._value[3:] = xyz
+        for i in range(3, 6):
+            if self._visible[i]:
+                self._meshcat.SetSliderValue(self._visible._fields[i],
+                                             self._value[i])
+
+    def _update_values(self):
+        changed = False
+        for i in range(6):
+            if self._visible[i]:
+                old_value = self._value[i]
+                self._value[i] = self._meshcat.GetSliderValue(
+                    self._visible._fields[i])
+                changed = changed or self._value[i] != old_value
+        return changed
+
+    def _get_transform(self):
+        return RigidTransform(
+            RollPitchYaw(self._value[0], self._value[1], self._value[2]),
+            self._value[3:])
+
+    def DoCalcOutput(self, context, output):
+        """Constructs the output values from the sliders."""
+        self._update_values()
+        output.set_value(self._get_transform())
+
+    def Run(self, publishing_system, root_context, callback):
+        # Calls callback(root_context, pose), then publishing_system.Publish()
+        # each time the sliders change value.
+        # if not running_as_notebook:
+            # return
+
+        publishing_context = publishing_system.GetMyContextFromRoot(
+            root_context)
+
+        print("Press the 'Stop PoseSliders' button in Meshcat to continue.")
+        self._meshcat.AddButton("Stop PoseSliders")
+        while self._meshcat.GetButtonClicks("Stop PoseSliders") < 1:
+            if self._update_values():
+                callback(root_context, self._get_transform())
+                publishing_system.Publish(publishing_context)
+            time.sleep(.1)
+
+        self._meshcat.DeleteButton("Stop PoseSliders")
