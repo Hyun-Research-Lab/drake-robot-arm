aad6692a6ca409023fd691da486f784937775859
Logan Dihel <logan.dihel@gmail.com>
2023-09-19 13:18:45 -0400

    Cylinder SDF simulate

--- scripts/cylinder_simulate.py
+++ scripts/cylinder_simulate.py
@@ -10,54 +10,11 @@ from pydrake.systems.analysis import Simulator
 from pydrake.systems.framework import DiagramBuilder
 from pydrake.visualization import AddDefaultVisualization, ModelVisualizer
 
-# Define a simple cylinder model.
-cylinder_sdf = """<?xml version="1.0"?>
-<sdf version="1.7">
-  <model name="cylinder">
-    <pose>0 0 0 0 0 0</pose>
-    <link name="cylinder_link">
-      <inertial>
-        <mass>1.0</mass>
-        <inertia>
-          <ixx>0.005833</ixx>
-          <ixy>0.0</ixy>
-          <ixz>0.0</ixz>
-          <iyy>0.005833</iyy>
-          <iyz>0.0</iyz>
-          <izz>0.005</izz>
-        </inertia>
-      </inertial>
-      <collision name="collision">
-        <geometry>
-          <cylinder>
-            <radius>0.1</radius>
-            <length>0.2</length>
-          </cylinder>
-        </geometry>
-      </collision>
-      <visual name="visual">
-        <geometry>
-          <cylinder>
-            <radius>0.1</radius>
-            <length>0.2</length>
-          </cylinder>
-        </geometry>
-        <material>
-          <diffuse>1.0 1.0 1.0 1.0</diffuse>
-        </material>
-      </visual>
-    </link>
-  </model>
-</sdf>
-"""
-True
-
 meshcat = StartMeshcat()
 meshcat.AddButton('say_hello')
 
-# Visualize the cylinder from the SDFormat string you just defined.
 visualizer = ModelVisualizer(meshcat=meshcat)
-visualizer.parser().AddModelsFromString(cylinder_sdf, "sdf")
+visualizer.parser().AddModels("./sdf/cylinder.sdf")
 
 # Click the "Stop Running" button in MeshCat when you're finished.
 visualizer.Run(loop_once=False)
