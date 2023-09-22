4215cc38162a7fba90c3b4f7cc99838d3bd4298d
Logan Dihel <logan.dihel@gmail.com>
2023-09-22 15:12:59 -0400

    Double integrator with geometry working!

--- scripts/block_din.py
+++ scripts/block_din.py
@@ -1,25 +1,15 @@
 import numpy as np
-from pydrake.common.containers import namedview
-from pydrake.common.value import Value
+import matplotlib.pyplot as plt
+
 from pydrake.math import RigidTransform, RotationMatrix
 from pydrake.systems.analysis import Simulator
 from pydrake.systems.framework import BasicVector, Context, LeafSystem, SystemOutput
+from pydrake.geometry import Box, GeometryFrame, FramePoseVector, GeometryInstance, IllustrationProperties
+from pydrake.common.value import AbstractValue
 
-from copy import copy
-import matplotlib.pyplot as plt
-import numpy as np
 from pydrake.all import (
     DiagramBuilder,
-    MeshcatVisualizer,
     LogVectorOutput,
-    Saturation,
-    SceneGraph,
-    Simulator,
-    VectorSystem
-)
-
-from pydrake.all import (
-    DiagramBuilder,
     MeshcatVisualizer,
     RigidTransform,
     RotationMatrix,
@@ -31,7 +21,7 @@ from pydrake.all import (
 class BlockDin(LeafSystem):
     def __init__(self):
         LeafSystem.__init__(self)
-        self.DeclareVectorInputPort("u_din", 1)
+        self.u_port = self.DeclareVectorInputPort("u_din", 1)
         state_index = self.DeclareContinuousState(1,1,0) # Two state variables, x and xdot
         # you must use DeclareContinuousState(1,1,0)
         # AND NOT DeclareContinuousState(2) because this function requires 3 inputs:
@@ -51,23 +41,42 @@ class BlockDin(LeafSystem):
         x = context.get_continuous_state_vector().CopyToVector()
 
         # get input port from context
-        u = self.EvalVectorInput(context, 0).CopyToVector()[0]
+        u = self.u_port.Eval(context)[0]
 
+        # state space model
         xdot = x[1]
         xddot = u
 
         # set the derivative of xdot equal to xddot
         derivatives.get_mutable_vector().SetFromVector(np.array([xdot,xddot]))
 
+class BoxVisualizer(LeafSystem):
+    def __init__(self,frame_id):
+        LeafSystem.__init__(self)
+        self.frame_id = frame_id
+        self.x_port = self.DeclareVectorInputPort("x", BasicVector(2)) # position and velocity from BlockDin
+        self.DeclareAbstractOutputPort("my_pose", lambda: AbstractValue.Make(FramePoseVector()), self.CalcFramePoseOutput)
+        # YOU MUST DECLARE OUTPUT PORT AS AN ABSTRACT OUTPUT PORT
+        # do not use a vector port, as the scene_graph input expects a frameposevector
+        # see https://github.com/RussTedrake/drake/blob/c185d5dd5321ce59fb6c298e1f5e412f55750d6b/examples/acrobot/acrobot_geometry.cc#L55
+        # see https://github.com/RussTedrake/drake/blob/c185d5dd5321ce59fb6c298e1f5e412f55750d6b/bindings/pydrake/systems/jupyter_widgets_examples.ipynb#L53
+
+    def CalcFramePoseOutput(self, context, output):
+        position = self.x_port.Eval(context)[0]
+        output.get_mutable_value().set_value(self.frame_id, RigidTransform(
+            RotationMatrix().Identity(),
+            np.array([position,0,0])
+        ))
+
 class StupidController(LeafSystem):
     def __init__(self):
-        super().__init__()
+        LeafSystem.__init__(self)
         self.DeclareVectorOutputPort("y_control", 1, calc=self.CalcOutput)
-        self.DeclareVectorInputPort("u_control", 2)
+        self.u_port = self.DeclareVectorInputPort("u_control", 2)
 
     def CalcOutput(self, context, output):
         # u is the position of the block
-        u = self.EvalVectorInput(context, 0).CopyToVector()[0]
+        u = self.u_port.Eval(context)[0]
         y = -1 if u > 0 else 1
         output.SetAtIndex(0, y)
 
@@ -90,10 +99,10 @@ def main():
     # Set up a simulator to run this diagram
     simulator = Simulator(diagram)
     context = simulator.get_mutable_context()
-    # simulator.set_target_realtime_rate(1.0)
 
+    # set initial conditions
     plant_context = diagram.GetMutableSubsystemContext(plant, context)
-    plant_context.get_mutable_continuous_state_vector().SetFromVector([np.pi/2, 0.0])
+    plant_context.get_mutable_continuous_state_vector().SetFromVector([1.0, 0.0])
 
     # Run the simulation.
     simulator.Initialize()
@@ -115,7 +124,7 @@ def main():
     plt.grid()
     plt.show()
 
-def main_meshcat():
+def main_meshcat(render_mode_2D=False):
     # create the diagram
     builder = DiagramBuilder()
     plant = builder.AddSystem(BlockDin())
@@ -124,18 +133,29 @@ def main_meshcat():
     # Setup visualization
     scene_graph = builder.AddSystem(SceneGraph())
     meshcat = StartMeshcat()
+
+    # create a frame
+    source_id = scene_graph.RegisterSource("my_block_source")
+    frame_id = scene_graph.RegisterFrame(source_id, GeometryFrame("my_frame", 0))
+    geometry_id = scene_graph.RegisterGeometry(source_id, frame_id, 
+        GeometryInstance(RigidTransform(), Box(1.0, 1.0, 1.0), "my_geometry_instance"))
+
+    # must assign an illustration role to see the geometry
+    scene_graph.AssignRole(source_id, geometry_id, IllustrationProperties())
+
     MeshcatVisualizer.AddToBuilder(builder, scene_graph, meshcat)
-    meshcat.Set2dRenderMode(
-        X_WC=RigidTransform(RotationMatrix.MakeZRotation(np.pi), [0, 1, 0])
-    )
+    
+    if render_mode_2D:
+        meshcat.Set2dRenderMode(X_WC=RigidTransform(RotationMatrix.MakeZRotation(np.pi), [0, 0, 0]))
+
+    box_viz = builder.AddSystem(BoxVisualizer(frame_id))
 
     # wire it all up
     builder.Connect(plant.get_output_port(0), controller.get_input_port(0))
     builder.Connect(controller.get_output_port(0), plant.get_input_port(0))
 
-    # create logger_plant to capture the plant output
-    logger_plant = LogVectorOutput(plant.get_output_port(0), builder)
-    logger_controller = LogVectorOutput(controller.get_output_port(0), builder)
+    builder.Connect(plant.get_output_port(0), box_viz.get_input_port(0))
+    builder.Connect(box_viz.get_output_port(0), scene_graph.get_source_pose_port(source_id))
 
     diagram = builder.Build()
 
@@ -143,8 +163,9 @@ def main_meshcat():
     simulator = Simulator(diagram)
     context = simulator.get_mutable_context()
 
+    # set the block initial conditions
     plant_context = diagram.GetMutableSubsystemContext(plant, context)
-    plant_context.get_mutable_continuous_state_vector().SetFromVector([np.pi/2, 0.0])
+    plant_context.get_mutable_continuous_state_vector().SetFromVector([1.0, 0.0])
 
     # Run the simulation.
     simulator.set_target_realtime_rate(1.0)
@@ -152,16 +173,14 @@ def main_meshcat():
 
     meshcat.StartRecording()
     simulator.Initialize()
-    simulator.AdvanceTo(10.0)
+    simulator.AdvanceTo(30.0)
     meshcat.PublishRecording()
+
+    while True:
+        pass
 
 
 if __name__ == "__main__":
-    main()
-    #main_meshcat()
-
-
-
-
-
-
+    #main()
+    main_meshcat(render_mode_2D=False)
+    
