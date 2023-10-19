ecfc4c85340ae110dcfae3a2db5e2f223359f885
Logan Dihel <logan.dihel@gmail.com>
2023-10-19 13:54:09 -0400

    Use gamepad to move propeller guards

--- scripts/bicopter_multibody_urdf_gamepad.py
+++ scripts/bicopter_multibody_urdf_gamepad.py
@@ -79,23 +79,52 @@ def main():
 
     J1 = bicopter_model.GetJointByName("J1")
     J3 = bicopter_model.GetJointByName("J3")
-    J1.set_angle(context, 0.2)
-    J3.set_angle(context, -0.2)
+    #J1.set_angle(context, 0.2)
+    #J3.set_angle(context, -0.2)
+
     body = bicopter_model.GetBodyByName("body")
-    bicopter_model.SetFreeBodyPose(context, body, RigidTransform(RotationMatrix().MakeXRotation(1), [0, 0, 0.5]))
     bicopter_model.mutable_gravity_field().set_gravity_vector([0, 0, 0])
+    #bicopter_model.SetFreeBodyPose(context, body, RigidTransform(RotationMatrix().MakeXRotation(1), [0, 0, 0.5]))
+
+    # Gamepad
+    print('To connect gamepad, switch to browser tab with Meshcat (refresh if necessary), and then spam the A button on the gamepad.')
+    while(meshcat.GetGamepad().index == None):
+        pass
+    print('Connected!')
+    
     
     simulator = Simulator(diagram, root_context)
     simulator.Initialize()
 
     # meshcat.StartRecording()
     simulator.set_target_realtime_rate(1.0)
-    simulator.AdvanceTo(1.0)
     # meshcat.StopRecording()
     # meshcat.PublishRecording()
 
+    def deadzone(x):
+        y = np.zeros_like(x)
+        for i in range(len(x)):
+            if abs(x[i]) < 0.2:
+                y[i] = 0
+            else:
+                y[i] = x[i]
+        return y
+
     while True:
-        pass
+        gamepad = meshcat.GetGamepad()
+        axes = deadzone(gamepad.axes)
+        axes = gamepad.axes
+        print(axes)
+
+        # B button pressed
+        if gamepad.button_values[1] == 1:
+            pass
+            
+        J1.set_angle(context, axes[4])
+        J3.set_angle(context, axes[1])
+        
+        
+        simulator.AdvanceTo(simulator.get_context().get_time() + 1/60)
 
 if __name__ == "__main__":
     print('NOTE: Meshcat sometimes takes a long time to start up. Please be patient.')
