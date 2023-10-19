21bc68583b676caff7f5c378485e3a95d2ef7a2a
Logan Dihel <logan.dihel@gmail.com>
2023-10-19 13:20:54 -0400

    Refactored bicopter

--- scripts/bicopter_multibody_urdf_gamepad.py
+++ scripts/bicopter_multibody_urdf_gamepad.py
@@ -55,17 +55,17 @@ from pydrake.all import (
 
 def main():
     builder = DiagramBuilder()
-    plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=0)
-    Parser(plant).AddModels("./resources/bicopter.urdf")
-    plant.Finalize()
+    bicopter_model, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=0)
+    Parser(bicopter_model).AddModels("./resources/bicopter.urdf")
+    bicopter_model.Finalize()
 
     # For more detailed information, you can iterate through bodies, joints, and actuators:
-    for body_index in range(plant.num_bodies()):
-        body = plant.get_body(BodyIndex(body_index))
+    for body_index in range(bicopter_model.num_bodies()):
+        body = bicopter_model.get_body(BodyIndex(body_index))
         print(f"Body: {body.name()}, Model Instance: {body.model_instance()}")
 
-    for i in range(plant.num_joints()):
-        joint = plant.get_joint(JointIndex(i))
+    for i in range(bicopter_model.num_joints()):
+        joint = bicopter_model.get_joint(JointIndex(i))
         print(f"Joint: {joint.name()}, Model Instance: {joint.model_instance()}")
 
     meshcat = Meshcat()
@@ -75,23 +75,24 @@ def main():
     # https://github.com/RobotLocomotion/drake/blob/67671ab0c97be45434a8b6f9609901f685c55462/doc/_pages/troubleshooting.md
     # github.com/RobotLocomotion/drake/ --> drake/doc/_pages/troubleshooting.md
     root_context = diagram.CreateDefaultContext()
-    context = plant.GetMyContextFromRoot(root_context=root_context)
+    context = bicopter_model.GetMyContextFromRoot(root_context=root_context)
 
-    J1 = plant.GetJointByName("J1")
-    J3 = plant.GetJointByName("J3")
+    J1 = bicopter_model.GetJointByName("J1")
+    J3 = bicopter_model.GetJointByName("J3")
     J1.set_angle(context, 0.2)
     J3.set_angle(context, -0.2)
-    body = plant.GetBodyByName("body")
-    plant.SetFreeBodyPose(context, body, RigidTransform(RotationMatrix().MakeXRotation(1), [0, 0, 0.5]))
-    plant.mutable_gravity_field().set_gravity_vector([0, 0, 0])
+    body = bicopter_model.GetBodyByName("body")
+    bicopter_model.SetFreeBodyPose(context, body, RigidTransform(RotationMatrix().MakeXRotation(1), [0, 0, 0.5]))
+    bicopter_model.mutable_gravity_field().set_gravity_vector([0, 0, 0])
     
     simulator = Simulator(diagram, root_context)
     simulator.Initialize()
 
-    meshcat.StartRecording()
+    # meshcat.StartRecording()
     simulator.set_target_realtime_rate(1.0)
     simulator.AdvanceTo(1.0)
-    meshcat.StopRecording()
+    # meshcat.StopRecording()
+    # meshcat.PublishRecording()
 
     while True:
         pass
