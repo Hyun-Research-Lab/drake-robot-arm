63fe9b2494f8dbf63dca2244ee07fbc181d52cdc
Logan Dihel <logan.dihel@gmail.com>
2023-10-19 12:55:01 -0400

    Import URDF file, move joints, move body

---
+++ scripts/bicopter_multibody_urdf_gamepad.py
@@ -0,0 +1,96 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from pydrake.math import RigidTransform, RotationMatrix, ClosestQuaternion
+from pydrake.systems.analysis import Simulator
+from pydrake.systems.framework import BasicVector, Context, LeafSystem, SystemOutput
+from pydrake.geometry import Box, GeometryFrame, FramePoseVector, GeometryInstance, IllustrationProperties, Mesh
+from pydrake.common.value import AbstractValue
+from pydrake.common.eigen_geometry import Quaternion
+
+from pydrake.common import temp_directory
+from pydrake.geometry import StartMeshcat
+from pydrake.math import RigidTransform, RollPitchYaw
+from pydrake.multibody.parsing import Parser
+from pydrake.multibody.plant import AddMultibodyPlantSceneGraph
+from pydrake.systems.analysis import Simulator
+from pydrake.systems.framework import DiagramBuilder
+
+from copy import copy
+import matplotlib.pyplot as plt
+import numpy as np
+from pydrake.all import (
+    DiagramBuilder,
+    MeshcatVisualizer,
+    RigidTransform,
+    RotationMatrix,
+    DiagramBuilder,
+    MeshcatVisualizer,
+    LeafSystem,
+    Simulator,
+    Simulator,
+    CoulombFriction,
+    HalfSpace,
+    LogVectorOutput,
+    SpatialVelocity,
+    BasicVector,
+    AffineSystem,
+    ConstantVectorSource,
+)
+
+from pydrake.all import (
+    DiagramBuilder,
+    LogVectorOutput,
+    MeshcatVisualizer,
+    RigidTransform,
+    RotationMatrix,
+    SceneGraph,
+    Simulator,
+    Meshcat,
+    MultibodyPlant,
+    JointIndex,
+    BodyIndex,
+)
+
+
+def main():
+    builder = DiagramBuilder()
+    plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=0)
+    Parser(plant).AddModels("./resources/bicopter.urdf")
+    plant.Finalize()
+
+    # For more detailed information, you can iterate through bodies, joints, and actuators:
+    for body_index in range(plant.num_bodies()):
+        body = plant.get_body(BodyIndex(body_index))
+        print(f"Body: {body.name()}, Model Instance: {body.model_instance()}")
+
+    for i in range(plant.num_joints()):
+        joint = plant.get_joint(JointIndex(i))
+        print(f"Joint: {joint.name()}, Model Instance: {joint.model_instance()}")
+
+    meshcat = Meshcat()
+    MeshcatVisualizer.AddToBuilder(builder, scene_graph, meshcat)
+    diagram = builder.Build()
+
+    # https://github.com/RobotLocomotion/drake/blob/67671ab0c97be45434a8b6f9609901f685c55462/doc/_pages/troubleshooting.md
+    # github.com/RobotLocomotion/drake/ --> drake/doc/_pages/troubleshooting.md
+    root_context = diagram.CreateDefaultContext()
+    context = plant.GetMyContextFromRoot(root_context=root_context)
+
+    J1 = plant.GetJointByName("J1")
+    J3 = plant.GetJointByName("J3")
+    J1.set_angle(context, 0.2)
+    J3.set_angle(context, -0.2)
+    body = plant.GetBodyByName("body")
+    plant.SetFreeBodyPose(context, body, RigidTransform(RotationMatrix(), [0, 0, 0.5]))
+    
+    simulator = Simulator(diagram, root_context)
+    simulator.Initialize()
+    # simulator.AdvanceTo(1.0) # error
+
+    while True:
+        pass
+
+if __name__ == "__main__":
+    print('NOTE: Meshcat sometimes takes a long time to start up. Please be patient.')
+    main()
